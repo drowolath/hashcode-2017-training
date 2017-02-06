aab676771a73171ae0cb1bcca1db05ad98a30192
Thomas Ayih-Akakpo <thomas.ayih-akakpo@gulfsat.mg>
2017-02-06 19:13:26 +0300

    update

---
+++ README.rst
@@ -0,0 +1,86 @@
+====================
+Google Hashcode 2017
+====================
+
+Problem description
+===================
+
+Pizza
+-----
+
+The pizza is represented as a rectangular, 2-dimensional grid of R rows and C columns. The cells within the grid are referenced using a pair of 0-based coordinates [r, c] , denoting respectively the row and the column of the cell.
+
+Each cell of the pizza contains either:
+
+ * mushroom, represented in the input file as M ; or
+
+ * tomato, represented in the input file as T
+
+Slice
+-----   
+
+A slice of pizza is a rectangular section of the pizza delimited by two rows and two columns, without holes.
+
+The slices we want to cut out must contain at least L cells of each ingredient (that is, at least L cells of mushroom and at least L cells of tomato) and at most H cells of any kind in total - surprising as it is, there is such a thing as too much pizza in one slice.
+
+The slices being cut out cannot overlap. The slices being cut do not need to cover the entire pizza.
+
+Goal
+----
+
+The goal is to cut correct slices out of the pizza maximizing the total number of cells in all slices.
+
+Input data set
+==============
+
+The input data is provided as a data set file - a plain text file containing exclusively ASCII characters with lines terminated with a single ‘\n’ character at the end of each line (UNIX- style line endings).
+
+File format
+-----------
+The file consists of:
+
+ * one line containing the following natural numbers separated by single spaces:
+   * R (1 ≤ R ≤ 1000) is the number of rows,
+   * C (1 ≤ C ≤ 1000) is the number of columns,
+   * L (1 ≤ L ≤ 1000) is the minimum number of each ingredient cells in a slice,
+   * H (1 ≤ H ≤ 1000) is the maximum total number of cells of a slice
+
+ * R lines describing the rows of the pizza (one after another). Each of these lines contains C
+ characters describing the ingredients in the cells of the row (one cell after another). Each character is either ‘M’ (for mushroom) or ‘T’ (for tomato).
+
+Submissions
+===========
+
+File format
+-----------
+
+The file must consist of:
+  * one line containing a single natural number S (0 ≤ S ≤ R × C) , representing the total number of slices to be cut,
+  * U lines describing the slices. Each of these lines must contain the following natural numbers separated by single spaces:
+    * r1 , c1 , r2 , c2 (0 ≤ r1 ,r2 < R, 0 ≤ c1, c2 < C) describe a slice of pizza delimited by the rows r1 and r2 and the columns c1 and c2 , including the cells of the delimiting rows and columns. The rows (r1 and r2) can be given in any order. The columns (c1 and c2) can be given in any order too.
+
+Validation
+----------
+
+For the solution to be accepted:
+  * the format of the file must match the description above,
+  * each cell of the pizza must be included in at most one slice,
+  * each slice must contain at least L cells of mushroom,
+  * each slice must contain at least L cells of tomato,
+  * total area of each slice must be at most H
+    
+Scoring
+-------    
+The submission gets a score equal to the total number of cells in all slices
+ 
+Usage of solution
+=================
+
+Very straightforward:
+
+.. code-block:: python
+
+   >>> import pizza
+   >>> p = pizza.Pizza(data='/filepath/to/dataset')
+   >>> p.slices  # should return the maximized valid slices
+   >>> p.elementary_slices  # returns the valid elementary slices of the pizza
--- pizza.py
+++ pizza.py
@@ -1,28 +1,53 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+"""
+Simple library to cut correct slices out of a pizza,
+maximizing the total number of cells in all slices
+"""
 
-class Position(object):
-    """A position is recognized by its row and column indices"""
+
+def gcd(a, b):
+    """Calculates the greater common divisor of a and b.
+    This will be helpful to get the maximum size possible
+    of a slice.
+    Given the size of a pizza (RxC),
+    if the maximum size of slice is rxc=H,
+    then, a slice is either
+     * r = gcd((RxC), H) rows and c = H/gcd((RxC), H) columns,
+     * r = H/gcd((RxC), H) rows and c = H/ columns
+    """
+    if b == 0:
+        return a
+    else:
+        r = a % b
+        return gcd(b, r)
+
+
+class Cell(object):
+    """A cell is recognized by a pair of 0 based coordinates (r,c)
+    denoting respectively its row and its column"""
     def __init__(self, row, column):
         self.row = row
         self.column = column
 
     def neighbours(self, R, C):
-        """a position has at most 4 adjacent cells; we don't count diagonals"""
+        """A cell has at most 4 adjacent cells; we don't count diagonals.
+        R is the number of rows in the rectangle containing the cell,
+        anc C is the number of columns"""
         neighbours = []
         if self.row+1 < R:
-            neighbours.append(Position(self.row+1, self.column))
+            neighbours.append(Cell(self.row+1, self.column))
         if self.row-1 >= 0:
-            neighbours.append(Position(self.row-1, self.column))
+            neighbours.append(Cell(self.row-1, self.column))
         if self.column+1 < C:
-            neighbours.append(Position(self.row, self.column+1))
+            neighbours.append(Cell(self.row, self.column+1))
         if self.column-1 < C:
-            neighbours.append(Position(self.row, self.column-1))
+            neighbours.append(Cell(self.row, self.column-1))
         return neighbours
 
     def __repr__(self):
-        return '<Position ({0}, {1})>'.format(self.row, self.column)
+        return '<Cell ({0}, {1})>'.format(self.row, self.column)
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
@@ -41,20 +66,20 @@ class Position(object):
 
 
 class Ingredient(object):
-    """An ingredient has a count in a pizza, and at least one position"""
-    def __init__(self, count, positions):
+    """An ingredient has a count in a Pizza, and occupies cells (or not?)"""
+    def __init__(self, count, cells):
         self.count = count
-        self.positions = positions
+        self.cells = cells
 
     def __repr__(self):
-        return '<Ingredient {0}>'.format(self.positions)
+        return '<Ingredient {0}>'.format(self.cells)
 
 
 class Pizza(object):
-    """A pizza is represented by a RxC rectangle, and contains
-    tomatoes and mushrooms"""
+    """A pizza is represented by a RxC rectangle; each cell of the pizza
+    contains either a mushroom or a tomato"""
     def __init__(self, **kwargs):
-        if kwargs.get('data'):
+        if kwargs.get('data'):  # kwargs['data'] is the filepath to the dataset
             with open(kwargs['data']) as infile:
                 lines = infile.readlines()
                 self.rows, self.columns, self.L, self.H = [
@@ -71,9 +96,9 @@ class Pizza(object):
                     m += line.count('M')
                     for i in range(len(line)):
                         if line[i] == 'T':
-                            t_pos.append(Position(r, i))
+                            t_pos.append(Cell(r, i))
                         else:
-                            m_pos.append(Position(r, i))
+                            m_pos.append(Cell(r, i))
                     r += 1
                 self.tomatoes = Ingredient(t, t_pos)
                 self.mushrooms = Ingredient(m, m_pos)
@@ -88,26 +113,43 @@ class Pizza(object):
             t=self.tomatoes.count, m=self.mushrooms.count
             )
 
-    def slices(self):
-        """A slice contains at least 1 tomato and 1 mushroom.
-        This function gives all the slices containing exactly
-        1 mushroom and 1 tomato. It starts from the most rare
+    @property
+    def elementary_slices(self):
+        """An elementary slice in a pizza contains at least 1 of each
         ingredient"""
-        a = [
-            i for i in [self.tomatoes, self.mushrooms]
-            if i.count == min(self.tomatoes.count, self.mushrooms.count)
-            ][0]
-        if a == self.tomatoes:
-            b = self.mushrooms
-        else:
-            b = self.tomatoes
-        print(a, b)
         result = []
-        seen = []
-        for i in a.positions:
-            for j in i.neighbours(self.rows, self.columns):
-                if j in b.positions and j not in seen:
-                    result.append((i, j))
-                    seen.append(j)
-                    break
+        if not self.tomatoes.count or not self.mushrooms.count:
+            # the pizza contains only one ingredient;
+            # every cell is an elementary slice
+            for i in self.rows:
+                for j in self.columns:
+                    result.append([Cell(i, j), Cell(i, j)])
+        else:
+            a = [
+                i for i in [self.tomatoes, self.mushrooms]
+                if i.count == min(self.tomatoes.count, self.mushrooms.count)
+                ][0]
+            if a == self.tomatoes:
+                b = self.mushrooms
+            else:
+                b = self.tomatoes
+            result = []
+            seen = []
+            for i in a.cells:
+                for j in i.neighbours(self.rows, self.columns):
+                    if j in b.cells and j not in seen:
+                        result.append(sorted([i, j]))
+                        seen.append(j)
+                        break
         return result
+
+    def slices(self):
+        """We should recursively expand each elementary slice
+        to a maximum by including only free cells.
+        At the very end, when we have only valid slices,
+        we will use the gcd() to calculate how many column or row
+        we should add to the existing slices to maximize them
+        (e.g. grouping valid slices to a maximum)
+        """
+        pass
+# EOF
